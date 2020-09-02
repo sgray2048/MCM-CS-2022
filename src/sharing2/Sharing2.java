b760df874a228c9cecbbabef7941659ebf5553ef
sgray2048 <62411837+sgray2048@users.noreply.github.com>
2020-09-02 11:04:01 +0800

    Update Sharing2.java

--- src/sharing2/Sharing2.java
+++ src/sharing2/Sharing2.java
@@ -15,7 +15,7 @@ public class Sharing2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello you loser");
     }
     
 }
